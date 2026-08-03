340fd5af4c66e53386bad4c4270ce445f806f112
jiyaapanchall <panchaljiya790@gmail.com>
2026-08-03 18:08:31 +0530

    check sorted

---
+++ check sorted.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter elements:\n";
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    bool sorted = true;
+
+    for(int i = 0; i < n - 1; i++) {
+
+        if(arr[i] > arr[i + 1]) {
+
+            sorted = false;
+            break;
+
+        }
+
+    }
+
+    if(sorted)
+        cout << "\nArray is sorted.";
+    else
+        cout << "\nArray is not sorted.";
+
+    return 0;
+}
