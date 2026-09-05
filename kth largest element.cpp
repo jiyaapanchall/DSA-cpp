71574fc38368f0c5a2e45519716d309ec85be73a
jiyaapanchall <panchaljiya790@gmail.com>
2026-09-05 17:20:17 +0530

    kth largest element

---
+++ kth largest element.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int findKthLargest(vector<int>& nums, int k) {
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    for (int num : nums) {
+
+        minHeap.push(num);
+
+        // Keep only K elements
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+    }
+
+    return minHeap.top();
+}
+
+int main() {
+
+    vector<int> nums = {3, 2, 1, 5, 6, 4};
+
+    int k = 2;
+
+    cout << "Kth largest element: "
+         << findKthLargest(nums, k) << endl;
+
+    return 0;
+}
