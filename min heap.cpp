296a8a6f8413d2e079a89b322ff71046bb8987d2
jiyaapanchall <panchaljiya790@gmail.com>
2026-09-09 08:56:07 +0530

    min heap

---
+++ min heap.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    pq.push(10);
+    pq.push(30);
+    pq.push(20);
+    pq.push(5);
+    pq.push(40);
+
+    cout << "Top element: " << pq.top() << endl;
+
+    cout << "Elements: ";
+
+    while (!pq.empty()) {
+
+        cout << pq.top() << " ";
+
+        pq.pop();
+    }
+
+    return 0;
+}
