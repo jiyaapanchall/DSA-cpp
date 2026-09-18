d2e41360ca86ee29800c46b8274987925002a964
jiyaapanchall <panchaljiya790@gmail.com>
2026-09-18 14:50:07 +0530

    find the duplicate number

---
+++ find the duplicate number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int>& nums) {
+
+    // Phase 1: Find intersection point
+    int slow = nums[0];
+    int fast = nums[0];
+
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    // Phase 2: Find entrance of cycle
+    slow = nums[0];
+
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
+
+int main() {
+
+    vector<int> nums = {1, 3, 4, 2, 2};
+
+    cout << "Duplicate Number: "
+         << findDuplicate(nums);
+
+    return 0;
+}
